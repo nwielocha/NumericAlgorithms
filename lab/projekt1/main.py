e36e3ce8f907d51d0e091354cd0a005e71449b6a
Maciej <cym.mac@wp.pl>
2022-04-06 12:53:00 +0200

    essa

--- lab/projekt1/main.py
+++ lab/projekt1/main.py
@@ -2,15 +2,16 @@ import scipy.interpolate as ip
 import numpy as np
 from numpy.polynomial.polynomial import Polynomial
 import math
+from itertools import compress, product,combinations
 
 
-def sub_lists (l):
-    lists = []
-    for i in range(len(l) + 1):
-        for j in range(i):
-            lists.append(l[j: i])
-    return lists
-
+def sub_lists(a):
+    if len(a) == 0:
+        return [[]]
+    cs = []
+    for c in sub_lists(a[1:]):
+        cs += [c, c+[a[0]]]
+    return cs
 
 def return_polynomials(sub_arrays, data):
     result = []
@@ -25,11 +26,11 @@ if __name__ == '__main__':
     keys = [key for key, value in x_y.items()]
     values = [value for key, value in x_y.items()]
     subsequences_x = sub_lists(keys)
-
     array_polynomial = return_polynomials(subsequences_x, x_y)
     polynomials = [i[0] for i in array_polynomial]
 
     best_result = min(array_polynomial,key=lambda x: np.abs(constant - x[0](22)))
     best_subsequence = best_result[1]
     best_subsequence_polynomial = best_result[0]
-    print(f"Najlepiej estymująca grupa węzłów : \n\n{best_subsequence}\n\nWyliczony dla niej wielomian:\n\n{best_subsequence_polynomial}")
+    print(f"Najlepiej estymująca grupa węzłów : \n\n{best_subsequence}\n\nWyliczony dla niej wielomian:\n\n{best_subsequence_polynomial}\n\n"
+          f"Log22 = {constant} \nF(22) dla obliczonego wielomianu:{best_subsequence_polynomial(22)}")
