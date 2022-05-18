ddfc3a4f479fc4cbb8d5105bc60f44c2d7b5f016
Maciej <cym.mac@wp.pl>
2022-05-18 11:41:39 +0200

    nn

--- lab/projekt2/main.py
+++ lab/projekt2/main.py
@@ -8,7 +8,7 @@ class NewtonMethod:
         self.equation_2 = equation2
         self.point = Point(x_point, y_point)
 
-    def create_derivative_matrix(self):
+    def create_inverted_derivative_matrix(self) -> Matrix:
         """
         Creates matrix out of two equations passed through constructor.
         Matrix contains two derivatives of each passed equation - calculated by x and y variable
@@ -18,27 +18,44 @@ class NewtonMethod:
                        self.equation_1.diff(Symbol('y'))],
                       [self.equation_2.diff(Symbol('x')),
                        self.equation_2.diff(Symbol('y'))]])
-        return self.matrix
+        return self.matrix**-1
 
-    def sub_variables_for_values(self):
-        pass
+    def sub_variables_for_values(self,point: Point,matrix: Matrix) -> Matrix:
+        """
+        Substitute variables x,y with given Point coordinates
+        :param point: Point object
+        :param matrix: Matrix of equations which each element is going to be substituted with given values
+        :return: Matrix of values
+        """
+        for i in range (0,len(matrix)):
+            matrix[i] = matrix[i].subs(Symbol('x'),point.x).subs(Symbol('y'),point.y)
+        return matrix
 
-    def core_calculation(self,point: Point):
+    def core_calculation(self,point: Point) -> Matrix:
+        """
+        Function creates 3 components needed to calculate  next iteration point.
+        1st component - Matrix of x and y
+        2nd component - Matrix of equations derivatives substituted with x'y from given point
+        3rd component - Matrix of equations substituted with x'y from given component
+        :param point: Point which is used to perform calculation
+        :return: Point object used to calculate next values
+        """
         first_component = Matrix([[point.x],[point.y]])
-        second_component = self.create_derivative_matrix()
+        second_component = self.create_inverted_derivative_matrix()
         third_component = Matrix([[self.equation_1.subs(Symbol('x'),point.x).subs(Symbol('y'),point.y)],
                                   [self.equation_2.subs(Symbol('x'),point.x).subs(Symbol('y'),point.y)]])
         return first_component - second_component * third_component
 
 
 if __name__ == '__main__':
-    x,y = Symbol('x y')
+    x = Symbol('x')
+    y = Symbol('y')
     NewtonProcessor = NewtonMethod(4*x**2 + 9*y**2 - 16,
                                    2*y**2 + 4*y - x + 2,
                                    x_point=2,y_point=2)
 
-    p = NewtonProcessor.create_derivative_matrix()
-    for x in p.values():
-        print(x)
+    p = NewtonProcessor.create_inverted_derivative_matrix()
+    print(NewtonProcessor.sub_variables_for_values(Point(1,1),p))
+
 
 
